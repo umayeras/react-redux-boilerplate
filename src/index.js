e21ed3834ee2bb1d4ccc9556ab92592f9f972093
Umay Eras <umayeras@hotmail.com>
2020-06-18 13:25:59 +0300

    redux packages, actions and reducers folder added.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import Routes from './routes';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import Routes from "./routes";
+import Reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <Provider store={createStoreWithMiddleware(Reducers)}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
