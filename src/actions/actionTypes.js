5fe8629e71e44763a2b2e65c825bb93c6043c777
Umay Eras <umayeras@hotmail.com>
2020-06-19 14:21:14 +0300

    Create actionTypes.js
    FETCH_POSTS, FETCH_POST, CREATE_POST, UPDATE_POST, DELETE_POST properties added.

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,6 @@
+// Post
+export const FETCH_POSTS = "fetch_posts";
+export const FETCH_POST = "fetch_post";
+export const CREATE_POST = "create_post";
+export const UPDATE_POST = "update_post";
+export const DELETE_POST = "delete_post";
