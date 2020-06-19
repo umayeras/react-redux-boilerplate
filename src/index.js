f6c482d89def3eab51c9e689a090258f65781fea
Umay Eras <umayeras@hotmail.com>
2020-06-19 14:17:31 +0300

    Update index.js
    redux-thunk, redux-logger, redux-promise-middleware added.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import Routes from "./routes";
-import Reducers from "./reducers";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import promise from "redux-promise-middleware";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import Routes from "./routes";
+import rootReducer from "./reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(promise, thunk, logger))
+);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(Reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
       <Routes />
     </BrowserRouter>
